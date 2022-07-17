6c905a8132ed4a247f7333c5507f0fdda2230bc4
williameiji <william.fujimori@gmail.com>
2022-07-17 17:51:27 -0300

    refactor: signup components

---
+++ src/SignupStudents/SignupForm.js
@@ -0,0 +1,148 @@
+import styled from "styled-components";
+
+export default function SignupForm({
+	signupData,
+	handleSignupForm,
+	signupNewStudent,
+}) {
+	return (
+		<>
+			<form onSubmit={signupNewStudent}>
+				<p>CADASTRO</p>
+				<input
+					type="text"
+					name="date"
+					placeholder="Data de início"
+					onChange={(e) => handleSignupForm(e)}
+					value={signupData.date}
+					required
+				></input>
+				<input
+					type="text"
+					placeholder="Valor da Mensalidade"
+					name="value"
+					onChange={(e) => handleSignupForm(e)}
+					value={signupData.value}
+					required
+				></input>
+				<p>Dados do aluno:</p>
+				<input
+					type="text"
+					name="name"
+					placeholder="Nome do aluno"
+					onChange={(e) => handleSignupForm(e)}
+					value={signupData.name}
+					required
+				></input>
+				<BoxData>
+					<input
+						type="text"
+						name="cpfStudent"
+						placeholder="CPF"
+						onChange={(e) => handleSignupForm(e)}
+						value={signupData.cpfStudent}
+					></input>
+					<input
+						type="text"
+						name="rgStudent"
+						placeholder="RG"
+						onChange={(e) => handleSignupForm(e)}
+						value={signupData.rgStudent}
+					></input>
+				</BoxData>
+				<p>Caso menor:</p>
+				<input
+					type="text"
+					name="nameResp"
+					placeholder="Nome do responsável"
+					onChange={(e) => handleSignupForm(e)}
+					value={signupData.nameResp}
+				></input>
+				<BoxData>
+					<input
+						type="text"
+						name="cpfResp"
+						placeholder="CPF"
+						onChange={(e) => handleSignupForm(e)}
+						value={signupData.cpfResp}
+					></input>
+					<input
+						type="text"
+						name="rgResp"
+						placeholder="RG"
+						onChange={(e) => handleSignupForm(e)}
+						value={signupData.rgResp}
+					></input>
+				</BoxData>
+				<p>Dados de localização:</p>
+				<BoxAdress>
+					<input
+						type="text"
+						name="adress"
+						placeholder="Endereço"
+						onChange={(e) => handleSignupForm(e)}
+						value={signupData.adress}
+					></input>
+					<input
+						type="text"
+						name="number"
+						placeholder="Nº"
+						onChange={(e) => handleSignupForm(e)}
+						value={signupData.number}
+					></input>
+				</BoxAdress>
+				<input
+					type="text"
+					name="district"
+					placeholder="Bairro"
+					onChange={(e) => handleSignupForm(e)}
+					value={signupData.district}
+				></input>
+				<input
+					type="text"
+					name="city"
+					placeholder="Cidade"
+					onChange={(e) => handleSignupForm(e)}
+					value={signupData.city}
+				></input>
+				<input
+					type="text"
+					name="phone"
+					placeholder="Telefone"
+					onChange={(e) => handleSignupForm(e)}
+					value={signupData.phone}
+				></input>
+				<input
+					type="text"
+					name="email"
+					placeholder="E-mail"
+					onChange={(e) => handleSignupForm(e)}
+					value={signupData.email}
+				></input>
+				<button type="submit">Cadastrar</button>
+			</form>
+		</>
+	);
+}
+
+const BoxAdress = styled.span`
+	display: flex;
+	flex-direction: row;
+
+	input:first-child {
+		margin-right: 10px;
+	}
+
+	input:last-child {
+		width: 30%;
+	}
+`;
+
+const BoxData = styled.span`
+	display: flex;
+	flex-direction: row;
+
+	input:first-child {
+		margin-right: 10px;
+	}
+`;
--- src/SignupStudents/SignupStudents.js
+++ src/SignupStudents/SignupStudents.js
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import Home from "../components/Home/Home";
 import TestaCPF from "../components/Shared/checkCPF";
 import url from "../components/Services/url";
+import SignupForm from "./SignupForm";
 
 export default function SignupStudents() {
 	const [signupData, setSignupData] = useState({
@@ -54,120 +55,11 @@ export default function SignupStudents() {
 	return (
 		<Home>
 			<Box>
-				<form onSubmit={signupNewStudent}>
-					<p>CADASTRO</p>
-					<input
-						type="text"
-						name="date"
-						placeholder="Data de início"
-						onChange={(e) => handleSignupForm(e)}
-						value={signupData.date}
-						required
-					></input>
-					<input
-						type="text"
-						placeholder="Valor da Mensalidade"
-						name="value"
-						onChange={(e) => handleSignupForm(e)}
-						value={signupData.value}
-						required
-					></input>
-					<p>Dados do aluno:</p>
-					<input
-						type="text"
-						name="name"
-						placeholder="Nome do aluno"
-						onChange={(e) => handleSignupForm(e)}
-						value={signupData.name}
-						required
-					></input>
-					<BoxData>
-						<input
-							type="text"
-							name="cpfStudent"
-							placeholder="CPF"
-							onChange={(e) => handleSignupForm(e)}
-							value={signupData.cpfStudent}
-						></input>
-						<input
-							type="text"
-							name="rgStudent"
-							placeholder="RG"
-							onChange={(e) => handleSignupForm(e)}
-							value={signupData.rgStudent}
-						></input>
-					</BoxData>
-					<p>Caso menor:</p>
-					<input
-						type="text"
-						name="nameResp"
-						placeholder="Nome do responsável"
-						onChange={(e) => handleSignupForm(e)}
-						value={signupData.nameResp}
-					></input>
-					<BoxData>
-						<input
-							type="text"
-							name="cpfResp"
-							placeholder="CPF"
-							onChange={(e) => handleSignupForm(e)}
-							value={signupData.cpfResp}
-						></input>
-						<input
-							type="text"
-							name="rgResp"
-							placeholder="RG"
-							onChange={(e) => handleSignupForm(e)}
-							value={signupData.rgResp}
-						></input>
-					</BoxData>
-					<p>Dados de localização:</p>
-					<BoxAdress>
-						<input
-							type="text"
-							name="adress"
-							placeholder="Endereço"
-							onChange={(e) => handleSignupForm(e)}
-							value={signupData.adress}
-						></input>
-						<input
-							type="text"
-							name="number"
-							placeholder="Nº"
-							onChange={(e) => handleSignupForm(e)}
-							value={signupData.number}
-						></input>
-					</BoxAdress>
-					<input
-						type="text"
-						name="district"
-						placeholder="Bairro"
-						onChange={(e) => handleSignupForm(e)}
-						value={signupData.district}
-					></input>
-					<input
-						type="text"
-						name="city"
-						placeholder="Cidade"
-						onChange={(e) => handleSignupForm(e)}
-						value={signupData.city}
-					></input>
-					<input
-						type="text"
-						name="phone"
-						placeholder="Telefone"
-						onChange={(e) => handleSignupForm(e)}
-						value={signupData.phone}
-					></input>
-					<input
-						type="text"
-						name="email"
-						placeholder="E-mail"
-						onChange={(e) => handleSignupForm(e)}
-						value={signupData.email}
-					></input>
-					<button type="submit">Cadastrar</button>
-				</form>
+				<SignupForm
+					signupData={signupData}
+					handleSignupForm={handleSignupForm}
+					signupNewStudent={signupNewStudent}
+				/>
 			</Box>
 		</Home>
 	);
@@ -217,26 +109,5 @@ const Box = styled.div`
 		input:focus {
 			outline: none;
 		}
-	}
-`;
-const BoxAdress = styled.span`
-	display: flex;
-	flex-direction: row;
-
-	input:first-child {
-		margin-right: 10px;
-	}
-
-	input:last-child {
-		width: 30%;
-	}
-`;
-
-const BoxData = styled.span`
-	display: flex;
-	flex-direction: row;
-
-	input:first-child {
-		margin-right: 10px;
 	}
 `;

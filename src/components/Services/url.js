80c265660af6c27d01912b3a6dec1be81cf77c39
williameiji <william.fujimori@gmail.com>
2022-10-01 08:46:40 -0300

    feat: use url by env file

--- src/components/Services/url.js
+++ src/components/Services/url.js
@@ -1,10 +1,17 @@
-const BASE_URL = "https://born-backend.onrender.com";
+let BASE_URL;
+
+if (process.env.REACT_APP_MODE === "DEV") {
+	BASE_URL = process.env.REACT_APP_DEV_URI;
+} else {
+	BASE_URL = process.env.REACT_APP_BACKEND_URI;
+}
 
 const url = {
 	login: `${BASE_URL}/login`,
-	addStudent: `${BASE_URL}/signupStudents`,
-	searchStudent: `${BASE_URL}/searchstudents`,
-	editInfo: `${BASE_URL}/edit`,
+	addStudent: `${BASE_URL}/students`,
+	searchStudent: `${BASE_URL}/students/search/:name`,
+	editInfo: `${BASE_URL}/students/edit`,
+	deleteStudent: `${BASE_URL}/students/:id`,
 };
 
 export default url;

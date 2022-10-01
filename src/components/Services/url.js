8642116b5abbcc431536cac11c499147a303a3a6
williameiji <william.fujimori@gmail.com>
2022-10-01 09:04:06 -0300

    fix: endpoints

--- src/components/Services/url.js
+++ src/components/Services/url.js
@@ -9,9 +9,9 @@ if (process.env.REACT_APP_MODE === "DEV") {
 const url = {
 	login: `${BASE_URL}/login`,
 	addStudent: `${BASE_URL}/students`,
-	searchStudent: `${BASE_URL}/students/search/:name`,
+	searchStudent: `${BASE_URL}/students/search`,
 	editInfo: `${BASE_URL}/students/edit`,
-	deleteStudent: `${BASE_URL}/students/:id`,
+	deleteStudent: `${BASE_URL}/students`,
 };
 
 export default url;

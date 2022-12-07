134662625f5b724956f4ce15227598ee709f6d41
williameiji <william.fujimori@gmail.com>
2022-12-07 08:34:46 -0300

    feat: button component for side bar

---
+++ src/components/ButtonSideBar.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export default function ButtonSideBar({ text, handleClick, cyId }) {
+	return (
+		<>
+			<Button onClick={handleClick} data-cy={cyId}>
+				{text}
+			</Button>
+		</>
+	);
+}
+
+const Button = styled.button`
+	border: none;
+	border-radius: 10px 0 10px 0;
+	margin-top: 10px;
+	width: 200px;
+	padding: 15px;
+	background-color: #87ceeb;
+	font-weight: bold;
+
+	:hover {
+		background-color: darkblue;
+		color: white;
+		cursor: pointer;
+	}
+`;

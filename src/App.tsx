d3f87d556a091f8f9204373187dee88c495f6508
williameiji <william.fujimori@gmail.com>
2022-12-19 09:53:13 -0300

    feat: add protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
 
 import GlobalResetStyle from "./assets/css/GlobalResetStyle.css";
@@ -12,8 +12,8 @@ import Signup from "./pages/Signup";
 import Declaration from "./pages/Dashboard/Declaration";
 import MenuScreen from "./pages/Dashboard/MenuScreen";
 import PaymentScreen from "./pages/Dashboard/PaymentScreen";
-
 import Initial from "./pages/InitalScreen";
+import { getToken } from "./shared/getToken";
 
 import { UserProvider } from "./contexts/UserContext";
 import { ModalProvider } from "./contexts/ModalContext";
@@ -37,36 +37,65 @@ export default function App() {
 								<Route
 									path="/students"
 									element={
-										<SearchStudent
-											setRenderFinds={setRenderFinds}
-											renderFinds={renderFinds}
-										/>
+										<ProtectedRouteGuard>
+											<SearchStudent
+												setRenderFinds={setRenderFinds}
+												renderFinds={renderFinds}
+											/>
+										</ProtectedRouteGuard>
 									}
 								/>
 								<Route
 									path="/newstudent"
 									element={
-										<NewStudentsScreen setRenderFinds={setRenderFinds} />
+										<ProtectedRouteGuard>
+											<NewStudentsScreen setRenderFinds={setRenderFinds} />
+										</ProtectedRouteGuard>
 									}
 								/>
 								<Route
 									path="/studentinfo/:id"
 									element={
-										<MenuScreen
-											renderFinds={renderFinds}
-											setRenderFinds={setRenderFinds}
-										/>
+										<ProtectedRouteGuard>
+											<MenuScreen
+												renderFinds={renderFinds}
+												setRenderFinds={setRenderFinds}
+											/>
+										</ProtectedRouteGuard>
 									}
 								/>
 								<Route
 									path="/edit"
 									element={
-										<EditStudentInformation setRenderFinds={setRenderFinds} />
+										<ProtectedRouteGuard>
+											<EditStudentInformation setRenderFinds={setRenderFinds} />
+										</ProtectedRouteGuard>
 									}
 								/>
-								<Route path="/receipt" element={<Receipt />} />
-								<Route path="/declaration" element={<Declaration />} />
-								<Route path="/payment" element={<PaymentScreen />} />
+								<Route
+									path="/receipt"
+									element={
+										<ProtectedRouteGuard>
+											<Receipt />
+										</ProtectedRouteGuard>
+									}
+								/>
+								<Route
+									path="/declaration"
+									element={
+										<ProtectedRouteGuard>
+											<Declaration />
+										</ProtectedRouteGuard>
+									}
+								/>
+								<Route
+									path="/payment"
+									element={
+										<ProtectedRouteGuard>
+											<PaymentScreen />
+										</ProtectedRouteGuard>
+									}
+								/>
 							</Routes>
 						</ModalProvider>
 					</EditProvider>
@@ -74,4 +103,14 @@ export default function App() {
 			</BrowserRouter>
 		</>
 	);
+}
+
+function ProtectedRouteGuard(props: React.PropsWithChildren) {
+	const token = getToken();
+
+	if (!token) {
+		return <Navigate to="/login" />;
+	}
+
+	return <>{props.children}</>;
 }

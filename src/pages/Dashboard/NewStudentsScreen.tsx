b0cef41f72596580f6558f15f6556c20b491bd1b
williameiji <william.fujimori@gmail.com>
2022-12-20 10:48:03 -0300

    chore: remove authentication

--- src/pages/Dashboard/NewStudentsScreen.tsx
+++ src/pages/Dashboard/NewStudentsScreen.tsx
@@ -32,63 +32,43 @@ export default function NewStudentsScreen({
 
 	const modal = useContext(ModalContext);
 	const [isModalOpen, setIsModalOpen] = useState(false);
+	const informations = useContext(InformationContext);
 
 	const navigate = useNavigate();
-
-	let config = {
-		headers: {
-			Authorization: `Bearer ${getToken()}`,
-		},
-	};
 
 	async function submit(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault();
 		setIsModalOpen(true);
 
-		if (getToken()) {
-			try {
-				//disable for tests (must be a valid 'cpf')
-				if (
-					newStudentData.cpfStudent !== "" &&
-					!TestaCPF(newStudentData.cpfStudent)
-				) {
-					throw new Error("CPF do aluno inválido");
-				}
-
-				if (
-					newStudentData.cpfResp !== "" &&
-					!TestaCPF(newStudentData.cpfResp)
-				) {
-					throw new Error("CPF do responsável inválido");
-				}
-				//disable for tests (must be a valid 'cpf')
-
-				await addStudent(newStudentData, config);
-				modal?.setModalStatus({
-					status: "Sucesso!",
-					message: "Cadastro efetuado!",
-				});
-				setRenderFinds([]);
-
-				setTimeout(() => {
-					navigate("/students");
-				}, 2000);
-			} catch (err: any) {
-				modal?.setModalStatus({
-					status: "Error:",
-					message: err.response?.data || err.message,
-				});
-
-				if (err.response?.status === 401) {
-					setTimeout(() => {
-						setIsModalOpen(false);
-						navigate("/login");
-					}, 2000);
-				}
+		try {
+			//disable for tests (must be a valid 'cpf')
+			if (
+				newStudentData.cpfStudent !== "" &&
+				!TestaCPF(newStudentData.cpfStudent)
+			) {
+				throw new Error("CPF do aluno inválido");
 			}
-		} else {
-			alert("Você precisa estar logado!");
-			navigate("/login");
+
+			if (newStudentData.cpfResp !== "" && !TestaCPF(newStudentData.cpfResp)) {
+				throw new Error("CPF do responsável inválido");
+			}
+			//disable for tests (must be a valid 'cpf')
+
+			await addStudent(newStudentData, headerConfig());
+			modal?.setModalStatus({
+				status: "Sucesso!",
+				message: "Cadastro efetuado!",
+			});
+			informations.setRenderFinds([]);
+
+			setTimeout(() => {
+				navigate("/students");
+			}, 2000);
+		} catch (err: any) {
+			modal?.setModalStatus({
+				status: "Error:",
+				message: err.response?.data || err.message,
+			});
 		}
 	}
 

43ccc8dccb71a76fc3506392efd51315181511a8
williameiji <william.fujimori@gmail.com>
2022-12-23 10:00:09 -0300

    feat: add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useState } from "react";
+import { lazy, Suspense } from "react";
 
 import GlobalResetStyle from "./assets/css/GlobalResetStyle.css";
 import GlobalStyle from "./assets/css/GlobalStyle";
 import Login from "./pages/Login";
-import SearchStudent from "./pages/Dashboard/SearchStudent";
-import NewStudentsScreen from "./pages/Dashboard/NewStudentsScreen";
-import EditStudentInformation from "./pages/Dashboard/EditStudentInformation";
-import Receipt from "./pages/Dashboard/Receipt";
 import Signup from "./pages/Signup";
-import Declaration from "./pages/Dashboard/Declaration";
-import MenuScreen from "./pages/Dashboard/MenuScreen";
-import PaymentScreen from "./pages/Dashboard/PaymentScreen";
 import Initial from "./pages/InitalScreen";
 import { getToken } from "./shared/getToken";
+import Loading from "./shared/Loading";
 
 import { UserProvider } from "./contexts/UserContext";
 import { ModalProvider } from "./contexts/ModalContext";
 import { EditProvider } from "./contexts/EditInformationContext";
 import { InformationProvider } from "./contexts/InformationContext";
+
+const SearchStudent = lazy(() => import("./pages/Dashboard/SearchStudent"));
+const NewStudentsScreen = lazy(
+	() => import("./pages/Dashboard/NewStudentsScreen")
+);
+const MenuScreen = lazy(() => import("./pages/Dashboard/MenuScreen"));
+const EditStudentInformation = lazy(
+	() => import("./pages/Dashboard/EditStudentInformation")
+);
+const Declaration = lazy(() => import("./pages/Dashboard/Declaration"));
+const Receipt = lazy(() => import("./pages/Dashboard/Receipt"));
+const PaymentScreen = lazy(() => import("./pages/Dashboard/PaymentScreen"));
 
 export default function App() {
 	return (
@@ -34,6 +40,7 @@ export default function App() {
 									<Route path="/" element={<Initial />} />
 									<Route path="/login" element={<Login />} />
 									<Route path="/signup" element={<Signup />} />
+
 									<Route
 										path="/students"
 										element={
@@ -45,49 +52,61 @@ export default function App() {
 									<Route
 										path="/newstudent"
 										element={
-											<ProtectedRouteGuard>
-												<NewStudentsScreen />
-											</ProtectedRouteGuard>
+											<Suspense fallback={<Loading />}>
+												<ProtectedRouteGuard>
+													<NewStudentsScreen />
+												</ProtectedRouteGuard>
+											</Suspense>
 										}
 									/>
 									<Route
 										path="/studentinfo/:id"
 										element={
-											<ProtectedRouteGuard>
-												<MenuScreen />
-											</ProtectedRouteGuard>
+											<Suspense fallback={<Loading />}>
+												<ProtectedRouteGuard>
+													<MenuScreen />
+												</ProtectedRouteGuard>
+											</Suspense>
 										}
 									/>
 									<Route
 										path="/edit"
 										element={
-											<ProtectedRouteGuard>
-												<EditStudentInformation />
-											</ProtectedRouteGuard>
+											<Suspense fallback={<Loading />}>
+												<ProtectedRouteGuard>
+													<EditStudentInformation />
+												</ProtectedRouteGuard>
+											</Suspense>
 										}
 									/>
 									<Route
 										path="/receipt"
 										element={
-											<ProtectedRouteGuard>
-												<Receipt />
-											</ProtectedRouteGuard>
+											<Suspense fallback={<Loading />}>
+												<ProtectedRouteGuard>
+													<Receipt />
+												</ProtectedRouteGuard>
+											</Suspense>
 										}
 									/>
 									<Route
 										path="/declaration"
 										element={
-											<ProtectedRouteGuard>
-												<Declaration />
-											</ProtectedRouteGuard>
+											<Suspense fallback={<Loading />}>
+												<ProtectedRouteGuard>
+													<Declaration />
+												</ProtectedRouteGuard>
+											</Suspense>
 										}
 									/>
 									<Route
 										path="/payment"
 										element={
-											<ProtectedRouteGuard>
-												<PaymentScreen />
-											</ProtectedRouteGuard>
+											<Suspense fallback={<Loading />}>
+												<ProtectedRouteGuard>
+													<PaymentScreen />
+												</ProtectedRouteGuard>
+											</Suspense>
 										}
 									/>
 								</Routes>

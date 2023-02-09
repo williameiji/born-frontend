e8f05a40b60de14eebcb0457eac7fb75087f19b1
William <william.fujimori@gmail.com>
2023-02-09 15:35:44 -0300

    feat: add api to delete payments

--- src/services/paymentsApi.ts
+++ src/services/paymentsApi.ts
@@ -1,6 +1,13 @@
 import api from "./api";
 import { AxiosRequestConfig } from "axios";
 import { Payment } from "../layouts/Dashboard/Payment/types";
+import { getToken } from "../shared/getToken";
+
+let configHeader = {
+	headers: {
+		Authorization: `Bearer ${getToken()}`,
+	},
+};
 
 export async function getPaymentById(id: string) {
 	const { data } = await api.get(`/payments/${id}`);
@@ -12,4 +19,8 @@ export async function newPayment(
 	config: AxiosRequestConfig<any>
 ) {
 	await api.post("/payments", data, config);
+}
+
+export async function deletePayments(id: string) {
+	await api.delete(`/payments/${id}`, configHeader);
 }

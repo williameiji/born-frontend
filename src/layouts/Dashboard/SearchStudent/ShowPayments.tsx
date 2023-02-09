890e39b1cc0c54fa4480b23b54fdd18dea2af79a
William <william.fujimori@gmail.com>
2023-02-09 15:36:56 -0300

    feat: add button to delete a payment

--- src/layouts/Dashboard/SearchStudent/ShowPayments.tsx
+++ src/layouts/Dashboard/SearchStudent/ShowPayments.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 import ModalGeneric from "../../../shared/ModalGeneric";
 import ModalContext from "../../../contexts/ModalContext";
-import { getPaymentById } from "../../../services/paymentsApi";
+import { getPaymentById, deletePayments } from "../../../services/paymentsApi";
 import { TEditForm } from "../EditStudentInformation/types";
 import { TPayments } from "./types";
 
@@ -19,14 +20,23 @@ export default function ShowPayments({
 	const [isModalOpen, setIsModalOpen] = useState(false);
 	const modal = useContext(ModalContext);
 	const navigate = useNavigate();
+	const [control, setControl] = useState(false);
 
 	useEffect(() => {
 		setIsModalOpen(true);
 
 		(async () => {
 			try {
-				const response = await getPaymentById(renderFinds[Number(params)]._id);
-				setRenderPayments(response);
+				const response = await getPaymentById(
+					renderFinds[Number(params)]._id
+				);
+
+				if (response.length) {
+					setRenderPayments(response);
+				} else {
+					setRenderPayments([]);
+				}
+
 				setIsModalOpen(false);
 			} catch (error: any) {
 				modal?.setModalStatus({
@@ -38,19 +48,49 @@ export default function ShowPayments({
 				}, 2000);
 			}
 		})();
-	}, []);
+	}, [control]);
+
+	async function deletePayment(id: string) {
+		if (window.confirm("Deseja deletar o pagamento?")) {
+			setIsModalOpen(true);
+
+			try {
+				await deletePayments(id);
+				setIsModalOpen(false);
+				setControl(!control);
+			} catch (error: any) {
+				modal?.setModalStatus({
+					status: "Erro:",
+					message: error.response?.data,
+				});
+			}
+		}
+	}
 
 	return (
 		<>
-			<ModalGeneric isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+			<ModalGeneric
+				isModalOpen={isModalOpen}
+				setIsModalOpen={setIsModalOpen}
+			/>
 			<Box>
 				<ContainerPayments>
 					{renderPayments.length
 						? renderPayments.map((payment, index) => (
 								<RenderPayments key={index}>
 									<p>{payment.reference.toUpperCase()}</p>
-									<p>R${Number(payment.value).toFixed(2)}</p>{" "}
+									<p>
+										R${Number(payment.value).toFixed(2)}
+									</p>{" "}
 									<p>{payment.date}</p>
+									<BoxButtons>
+										<DeleteIcon
+											sx={{ color: "red" }}
+											onClick={() =>
+												deletePayment(payment._id)
+											}
+										/>
+									</BoxButtons>
 								</RenderPayments>
 						  ))
 						: "Aluno não realizou pagamentos ainda!"}
@@ -97,4 +137,8 @@ const RenderPayments = styled.div`
 	p {
 		width: 100%;
 	}
+`;
+
+const BoxButtons = styled.div`
+	display: flex;
 `;
